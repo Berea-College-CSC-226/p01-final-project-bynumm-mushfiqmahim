f1a8867451d9425669ae2214d08c56f2f48f551a
mahimm <mushfiquzzaman3@gmail.com>
2025-12-08 20:07:31 -0500

    Create test suite (initial unit test)

---
+++ src/test_snake.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import unittest
+
+sys.path.append(os.path.dirname(__file__))
+
+from snake import Snake
+
+
+class TestSnake(unittest.TestCase):
+    def test_initial_length_is_three(self):
+        """Snake should start with 3 segments."""
+        snake = Snake()
+        self.assertEqual(len(snake.segments), 3)
+
+    def test_move_changes_head_position(self):
+        """After one move, the head position should change."""
+        snake = Snake()
+        head_before = snake.segments[0]
+        snake.move()
+        head_after = snake.segments[0]
+        self.assertNotEqual(head_before, head_after)
+
+    def test_grow_increases_length_by_one(self):
+        """Calling grow() then move() should increase length by 1."""
+        snake = Snake()
+        original_length = len(snake.segments)
+
+        snake.grow()
+        snake.move()
+
+        self.assertEqual(len(snake.segments), original_length + 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
